adde2050054c745e5ca94ae07799bb87c0699679
hh <jj>
2023-07-11 00:05:31 +0700

    Set up mutil language

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,18 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:spotify_clone/presentation/resources/themes.dart';
 
-class TestPage extends StatelessWidget {
-  const TestPage({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [
-          Text('data')
-        ],
-      ),
+    return MaterialApp(
+      theme: AppThemes.lightTheme,
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      home: const Placeholder(),
     );
   }
 }
